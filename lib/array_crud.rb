1767598a383fbe7bf03a8da559b25de7ffdebc11
Seth Starr <starrseth@hotmail.com>
2020-06-08 01:25:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   video_games = ["nintendo", "playstation", "microsoft"]
-  video_games >> 
+  video_games << "pc"
 end
 
 def add_element_to_start_of_array(array, element)
