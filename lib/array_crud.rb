6116b1b3c181946f7f8c751864ef6cf35e9ce9b6
Seth Starr <starrseth@hotmail.com>
2020-06-08 01:24:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  video_games = ["nintendo"]
+  video_games = ["nintendo", "playstation", "microsoft"]
+  video_games >> 
 end
 
 def add_element_to_start_of_array(array, element)
